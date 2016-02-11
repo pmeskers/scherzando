5495b3e167599e1eddb111994a4be1b6286189aa
Paul Meskers <paulmeskers@gmail.com>
2016-02-11 14:47:26 -0500

    Increase the bar length

--- src/js/svg_components/music_bar.js
+++ src/js/svg_components/music_bar.js
@@ -1,15 +1,18 @@
 var React = require('react');
 var TrebleClef = require('./treble_clef.js');
 
+// I want to hardcode these values at some point, not sure it's valuable to keep them programmatic like this
+
 class MusicBar extends React.Component {
   render() {
     var gapWidth = 120;
     var topMargin = 50;
     var bottomMargin = 50;
     var leftMargin = 0;
+    var lineLength = 1200;
 
     var trueHeight = topMargin + (4 * gapWidth) + bottomMargin + 50;
-    var trueWidth = 796 + 35;
+    var trueWidth = lineLength + 25;
 
     return (
       <svg width={this.props.width} height={this.props.height} viewBox={`0 0 ${trueWidth} ${trueHeight}`}>
@@ -17,13 +20,13 @@ class MusicBar extends React.Component {
           <TrebleClef />
         </g>
         <g id='g1934'>
-          <path d={`M ${leftMargin},${topMargin} L 814,${topMargin}`} style={{stroke:'#131516', strokeWidth:'6.8'}} />
-          <path d={`M ${leftMargin},${topMargin + gapWidth * 1} L 814,${topMargin + gapWidth * 1}`} style={{stroke:'#131516', strokeWidth:'6.8'}} />
-          <path d={`M ${leftMargin},${topMargin + gapWidth * 2} L 814,${topMargin + gapWidth * 2}`} style={{stroke:'#131516', strokeWidth:'6.8'}} />
-          <path d={`M ${leftMargin},${topMargin + gapWidth * 3} L 814,${topMargin + gapWidth * 3}`} style={{stroke:'#131516', strokeWidth:'6.8'}} />
-          <path d={`M ${leftMargin},${topMargin + gapWidth * 4} L 814,${topMargin + gapWidth * 4}`} style={{stroke:'#131516', strokeWidth:'6.8'}} />
-          <path d={`M 752,${topMargin} L 752,${topMargin + gapWidth * 4}`} style={{stroke:'#000000', strokeWidth:'6.5'}} id='path4032'/>
-          <path d={`M 796,${topMargin} L 796,${topMargin + gapWidth * 4}`} style={{stroke:'#000000', strokeWidth:'35'}} id='path4032'/>
+          <path d={`M ${leftMargin},${topMargin} L ${lineLength},${topMargin}`} style={{stroke:'#131516', strokeWidth:'6.8'}} />
+          <path d={`M ${leftMargin},${topMargin + gapWidth * 1} L ${lineLength},${topMargin + gapWidth * 1}`} style={{stroke:'#131516', strokeWidth:'6.8'}} />
+          <path d={`M ${leftMargin},${topMargin + gapWidth * 2} L ${lineLength},${topMargin + gapWidth * 2}`} style={{stroke:'#131516', strokeWidth:'6.8'}} />
+          <path d={`M ${leftMargin},${topMargin + gapWidth * 3} L ${lineLength},${topMargin + gapWidth * 3}`} style={{stroke:'#131516', strokeWidth:'6.8'}} />
+          <path d={`M ${leftMargin},${topMargin + gapWidth * 4} L ${lineLength},${topMargin + gapWidth * 4}`} style={{stroke:'#131516', strokeWidth:'6.8'}} />
+          <path d={`M ${lineLength - 62},${topMargin} L ${lineLength - 62},${topMargin + gapWidth * 4}`} style={{stroke:'#000000', strokeWidth:'6.5'}} id='path4032'/>
+          <path d={`M ${lineLength - 18},${topMargin} L ${lineLength - 18},${topMargin + gapWidth * 4}`} style={{stroke:'#000000', strokeWidth:'35'}} id='path4032'/>
         </g>
       </svg>
     );
