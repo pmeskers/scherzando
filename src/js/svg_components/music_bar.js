39a80d6a64e37768c8b6d1761ca77e3f840f228d
Paul Meskers <paulmeskers@gmail.com>
2016-02-11 15:08:02 -0500

    Add some sharps

--- src/js/svg_components/music_bar.js
+++ src/js/svg_components/music_bar.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var TrebleClef = require('./treble_clef.js');
+var Sharp = require('./sharp.js');
 
 // I want to hardcode these values at some point, not sure it's valuable to keep them programmatic like this
 
@@ -14,12 +15,29 @@ class MusicBar extends React.Component {
     var trueHeight = topMargin + (4 * gapWidth) + bottomMargin + 50;
     var trueWidth = lineLength + 25;
 
+    var fSharp = (
+      <g transform="translate(0, -305)">
+        <Sharp />
+      </g>
+    );
+    var cSharp = (
+      <g transform="translate(120, -130)">
+        <Sharp />
+      </g>
+    );
+    var gSharp = (
+      <g transform="translate(240, -370)">
+        <Sharp />
+      </g>
+    );
+
     return (
       <svg width={this.props.width} height={this.props.height} viewBox={`0 0 ${trueWidth} ${trueHeight}`}>
-        <g id='g2004'>
+        <g>
           <TrebleClef />
         </g>
-        <g id='g1934'>
+
+        <g>
           <path d={`M ${leftMargin},${topMargin} L ${lineLength},${topMargin}`} style={{stroke:'#131516', strokeWidth:'6.8'}} />
           <path d={`M ${leftMargin},${topMargin + gapWidth * 1} L ${lineLength},${topMargin + gapWidth * 1}`} style={{stroke:'#131516', strokeWidth:'6.8'}} />
           <path d={`M ${leftMargin},${topMargin + gapWidth * 2} L ${lineLength},${topMargin + gapWidth * 2}`} style={{stroke:'#131516', strokeWidth:'6.8'}} />
@@ -27,6 +45,12 @@ class MusicBar extends React.Component {
           <path d={`M ${leftMargin},${topMargin + gapWidth * 4} L ${lineLength},${topMargin + gapWidth * 4}`} style={{stroke:'#131516', strokeWidth:'6.8'}} />
           <path d={`M ${lineLength - 62},${topMargin} L ${lineLength - 62},${topMargin + gapWidth * 4}`} style={{stroke:'#000000', strokeWidth:'6.5'}} id='path4032'/>
           <path d={`M ${lineLength - 18},${topMargin} L ${lineLength - 18},${topMargin + gapWidth * 4}`} style={{stroke:'#000000', strokeWidth:'35'}} id='path4032'/>
+        </g>
+
+        <g>
+          {fSharp}
+          {cSharp}
+          {gSharp}
         </g>
       </svg>
     );
